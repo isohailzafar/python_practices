9508de69f3e7894421787801719fbcb5855a54ff
Sohail Zafar <sohailzafar092@gmail.com>
2024-05-05 20:17:31 +0500

    Update practise31.py

--- practise31.py
+++ practise31.py
@@ -1,15 +1,6 @@
-# class Car:
-#     brand = None
-#     model = None
+#Basic Class and Object
+#Problem: Create a Car class with attributes like brand and model. Then create an instance of this class.
 
-
-
-# #Object
-# my_car = Car() #copy of class in my_car
-# print(my_car)
-
-#Real Method
-#self is used to make the connection between Car class and my_car variable which contains the object
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
@@ -18,6 +9,3 @@ class Car:
 my_car = Car("Toyota", "Corola")
 print(my_car.brand)
 print(my_car.model)
-my_new_car = Car("Tata", "Safari")
-print(my_new_car.brand)
-print(my_new_car.model)
