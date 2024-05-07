f94eaad8ae7414dce62d6f8c5008223462382dcf
Sohail Zafar <sohailzafar092@gmail.com>
2024-05-07 14:37:34 +0500

    Update practise33.py

--- practise33.py
+++ practise33.py
@@ -1,16 +1,22 @@
+#Inheritance
+#Problem: Create an ElectricCar class that inherits from the Car class and has an additional attribute battery_size.
+
 class Car:
     def __init__(self, brand, model):
         self.brand = brand
         self.model = model
 
-    def fullname(self):
-        return f"{self.brand} {self.model}"
+    def full_name(self):
+        return f"{self.brand} {self.model} {self.battery_size}"
 
 class ElectricCar(Car):
-    def __init__(self, brand, model, batterysize):
+    def __init__(self, brand, model, battery_size):
         super().__init__(brand, model)
-        self.batterysize = batterysize
+        self.battery_size = battery_size
     
+my_car = ElectricCar("Tesla", "S", "85kWh")
+# print(my_car.brand)
+# print(my_car.model)
+# print(my_car.battery_size)
 
-my_car = ElectricCar("Tesla", "X", "85kWH")
-print(my_car.fullname())
+print(my_car.full_name())
